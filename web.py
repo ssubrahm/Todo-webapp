b8991b67521c18205062db25193aa77909f110c3
Srinath <ssubrahm@gmail.com>
2023-09-18 15:33:32 +0530

    Added a Today text to the ToDo App

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import functions
+from datetime import date
 
 todos = functions.get_todos()
 
@@ -10,6 +11,10 @@ def add_todo():
     functions.write_todos(todos)
     st.session_state["new_todo"] = ""
 
+
+today = date.today().strftime("%B %d, %Y")
+today_stamp = f'<p style="font-size:26px; color:yellow;">{today} </p>'
+st.write(today_stamp, unsafe_allow_html=True)
 
 st.title("My Todo WebApp")
 st.subheader("A tool to increase my Productivity")
